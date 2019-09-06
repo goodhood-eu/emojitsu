ffc58cbc41721762600b248f856ff54d64e8600d
Max Degterev <max@degterev.me>
2019-09-06 18:22:56 +0200

    updated refs

--- tasks/utils/assets.js
+++ tasks/utils/assets.js
@@ -1,6 +1,6 @@
 const pkg = require('../../package');
 
-const version = pkg.devDependencies['emojione-assets'].replace(/\^/g, '');
+const version = pkg.devDependencies['emoji-assets'].replace(/\^/g, '');
 
 const utils = {
   getVersion() {
